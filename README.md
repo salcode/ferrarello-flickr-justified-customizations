8be79f44567e0ad85c1f5073b4cb53324116d215
Sal Ferrarello <salcode@salogic.net>
2015-08-13 15:26:41 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+Ferrarello Flickr Justified Customizations
+==========================================
+
+I've started using the awesome
+[Flickr Justified Gallery](https://wordpress.org/plugins/flickr-justified-gallery/)
+for displaying the Flickr photos on my family site,
+[ferrarello.com](http://ferrarello.com/).
+
+While this plugin is great because of how the site was setup previously
+and the way the data is stored (in post_meta), I wanted to make the
+transistion as seamless as possible.
+
+This plugin extends Flickr Justified Gallery to:
+- read the existing post_meta (salogic_flickr_list) on the posts for the Flickr set IDs
+- read the existing post_meta (salogic_youtube_list) on the posts for the YouTube IDs
+- append the YouTube videos to the Flickr Gallery
+
+Support
+-------
+
+This code is entirely selfishly for my own benefit.  I've made it public only
+because you may find it interesting to spark your own ideas.  I am not
+providing any support.
---
+++ ferrarello-flickr-justified-customizations.php
@@ -0,0 +1,74 @@
+<?php
+/*
+ * Plugin Name: Ferrarello Flickr Justified Customizations
+ * Description: Modifications to Flickr Justified for use on Ferrarello.com.
+ * Version: 1.0.0
+ * Author: Sal Ferrarello
+ * Author URI: http://salferrarello.com/
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+class Ferrarello_Flickr_Justified_Customizations {
+	public $flickr_ids, $youtube_videos;
+
+	function __construct() {
+		add_filter( 'the_content', array( $this, 'the_content_filter' ) );
+	}
+
+	function the_content_filter( $content ) {
+		if ( ! $this->is_flickr_post() ) {
+			return $content;
+		}
+
+		$post_id = get_the_ID();
+
+		$this->flickr_ids =     explode( ',', get_post_meta( $post_id, 'salogic_flickr_list',  true ) );
+		$this->youtube_videos = explode( ',', get_post_meta( $post_id, 'salogic_youtube_list', true ) );
+
+		$this->add_youtube_videos_filter_setup();
+
+		foreach ( $this->flickr_ids as $flickr_id ) {
+			$content .= fjgwpp_flickr_set( array(
+				'id' => $flickr_id,
+			) );
+		}
+
+		return $content;
+	}
+
+	function add_youtube_videos_filter_setup() {
+		add_filter( 'fjgwpp_add_entries', array( $this, 'fjgwpp_add_entries_filter' ), 10, 4 );
+	}
+
+	function fjgwpp_add_entries_filter( $add_entries_content, $id, $lightbox, $flickrGalID ) {
+		foreach( $this->youtube_videos as $youtube_id ) {
+			$add_entries_content .= $this->youtube_markup( $youtube_id, $id, $lightbox, $flickrGalID );
+		}
+		remove_filter( 'fjgwpp_add_entries', array( $this, 'fjgwpp_add_entries_filter' ) );
+		return $add_entries_content;
+	}
+
+	function youtube_markup( $youtube_id, $id, $lightbox, $flickrGalID ) {
+		return '<a class="fe-youtube-video ' . $lightbox.'-video"'
+			. ' rel="' . $flickrGalID . '"'
+			. ' href="https://youtu.be/' . $youtube_id . '">'
+			. '<img data-skip-size-range-suffixes="true"'
+			. ' src="http://img.youtube.com/vi/' . $youtube_id . '/0.jpg"'
+			. ' alt="Video"></a>';
+	}
+
+	function is_flickr_post() {
+		$post_id = get_the_ID();
+		return (
+			'post' === get_post_type( $post_id )
+			&& in_category( 'ferrarello-boys', $post_id )
+		);
+	}
+
+}
+
+new Ferrarello_Flickr_Justified_Customizations;
